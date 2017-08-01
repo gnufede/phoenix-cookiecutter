4875b1729bd8a80be5abc368b49ca9f866f568fd
Fede <gnufede@gmail.com>
2017-08-01 12:13:01 +0200

    Update docker-bootstrap.sh

--- scripts/docker-bootstrap.sh
+++ scripts/docker-bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/local/bin/mix phx.new /opt/app/$PROJ_NAME
+/usr/local/bin/mix phx.new /opt/app/$PROJ_NAME < yes
 cd /opt/app/$PROJ_NAME/assets && npm install && node node_modules/brunch/bin/brunch build
